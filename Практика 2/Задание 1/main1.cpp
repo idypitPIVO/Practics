2e8f7d21cc9d36721f9502950df3edd24133ad5e
idypitPIVO <125370481+idypitPIVO@users.noreply.github.com>
2023-02-27 09:10:27 +0300

    Update main1.cpp

---
+++ Практика 2/Задание 1/main1.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include "triangle.h"
+int main(){
+    Triangle mas[3];
+    double a, b, c;
+    for (int i=0; i<3; i++) {
+        cout << "Введите a, b и c для треугольника № " << i+1 << " через пробел соответственно: " << endl;
+        cin >> a >> b >> c;
+        mas[i].set (a, b, c);
+        if (!(mas[i].exst_tr())) {
+            mas[i].show();
+            cout << "Треугольника с данными сторонами не существует, попробуйте ещё раз" << endl;
+            i--;
+        }
+    }
+    for (int i=0; i<3; i++) {
+        mas[i].show();
+        cout << "Периметр треугольника: " << mas[i].perimetr() << endl;
+        cout << "Площадь треугольника: " << mas[i].square() << endl;
+    }
+    return 0;
+}
